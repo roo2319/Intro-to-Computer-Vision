a8f327ab7100d19189fa84e71b64645c585fe1d6
RuairiF <rf17160@bris.ac.uk>
2019-11-18 21:52:13 +0000

    Fixed style and features of face.py

--- CW/face.py
+++ CW/face.py
@@ -3,24 +3,54 @@ import cv2
 import argparse
 import matplotlib.pyplot as plt
 import sys
+import os
+
+cascade_name = "frontalface.xml"
+cascade = cv2.CascadeClassifier()
+
+
 def detectAndDisplay(frame):
+
+    # 1. Prepeare the image by turning it grayscale and
+    #    normalising lighting.
     frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     frame_gray = cv2.equalizeHist(frame_gray)
-    #-- Detect faces
-    haar=cv2.CASCADE_SCALE_IMAGE
-    faces = face_cascade.detectMultiScale(frame_gray,1.1,1,0|haar, (50, 50), (500,500))
-    for (x,y,w,h) in faces:
-        center = (x + w//2, y + h//2)
-        frame = cv2.ellipse(frame, center, (w//2, h//2), 0, 0, 360, (255, 0, 255), 4)
-        faceROI = frame_gray[y:y+h,x:x+w]
+
+    # 2. Perform Viola-Jones object detection
+    faces = cascade.detectMultiScale(
+        frame_gray, 1.1, 1, 0 | cv2.CASCADE_SCALE_IMAGE, (50, 50), (500, 500))
+
+    # 3. Print the number of faces found
+    print(len(faces))
+
+    # 4. Draw boxes around the faces found
+    for (x, y, width, height) in faces:
+        frame = cv2.rectangle(
+            frame, (x, y), (x+width, y+height), (0, 255, 0), 2)
+
     cv2.imshow('Capture - Face detection', frame)
     cv2.waitKey(0)
-cv2.samples.addSamplesDataSearchPath("C:/Users/Theano/Desktop/Into-to-Computer-Vision/CW")
-face_cascade_name = "frontalface.xml"
-face_cascade = cv2.CascadeClassifier()
-#-- 1. Load the cascades
-if not face_cascade.load(cv2.samples.findFile(face_cascade_name)):
-    print('--(!)Error loading face cascade')
-    exit(0)
-frame=cv2.imread(sys.argv[1])
-detectAndDisplay(frame)
+
+
+# Takes a single argument of the image we are trying to detect faces on
+def main():
+
+    # 1. Read the Input Image
+    frame = cv2.imread(sys.argv[1])
+
+    # 2. Load the strong classifier
+    if not cascade.load(cascade_name):
+        print('--(!)Error loading face cascade')
+        exit(0)
+
+    # 3. Detect faces and display the result
+    detectAndDisplay(frame)
+
+    # 4. Save result image
+    cv2.imwrite("detected.jpg", frame)
+
+    return 0
+
+
+if __name__ == "__main__":
+    main()
